d74768378e4a725f38b062f5bec71ec8670430a4
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-05 18:47:35 -0400

    feat: add product and register routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './_pages/home/home.component';
+import { RegisterComponent } from './_pages/register/register.component';
+import { authGuard } from './_guards/auth.guard';
+import { ProductListComponent } from './_pages/product-list/product-list.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'products',
+        component: ProductListComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
