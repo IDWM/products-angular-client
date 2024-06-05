fb39aaa45cc6bdcc110656e18b7ec5bed41e4e88
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-05 18:46:42 -0400

    feat: add imports and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
 import { HomeComponent } from './_pages/home/home.component';
+import { RegisterComponent } from './_pages/register/register.component';
+import { TextInputComponent } from './_components/text-input/text-input.component';
+import { SelectInputComponent } from './_components/select-input/select-input.component';
+import { ProductListComponent } from './_pages/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent,
+    TextInputComponent,
+    SelectInputComponent,
+    ProductListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
