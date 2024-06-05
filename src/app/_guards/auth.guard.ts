b8dae7f757a231a3e73e5087862714060a85157d
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-05 18:45:34 -0400

    fix: remove code comments

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -3,12 +3,12 @@ import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../_services/auth.service';
 import { map } from 'rxjs';
 
-// export const authGuard: CanActivateFn = (_route, _state) => {
-//   const authService = inject(AuthService);
+export const authGuard: CanActivateFn = (_route, _state) => {
+  const authService = inject(AuthService);
 
-//   return authService.currentAuth$.pipe(
-//     map((auth) => {
-//       return auth != null;
-//     })
-//   );
-// };
+  return authService.currentAuth$.pipe(
+    map((auth) => {
+      return auth != null;
+    })
+  );
+};
