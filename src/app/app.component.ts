1df0d037f85089c803aff3a0556d0fa20cf7bc1d
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-06-05 18:47:56 -0400

    feat: initialize current auth from local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { AuthService } from './_services/auth.service';
+import { Auth } from './_interfaces/auth';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,18 @@ import { initFlowbite } from 'flowbite';
 export class AppComponent implements OnInit {
   title = 'products-angular-client';
 
+  constructor(private authService: AuthService) {}
+
   ngOnInit(): void {
     initFlowbite();
+    this.setCurrentAuth();
+  }
+
+  setCurrentAuth() {
+    const authString = localStorage.getItem('auth');
+    console.log('auth', authString);
+    if (!authString) return;
+    const auth: Auth = JSON.parse(authString);
+    this.authService.setCurrentAuth(auth);
   }
 }
